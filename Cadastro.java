8375f2685612eb2240ab6df47059984817664134
RicardoDeLima821 <ricardo.l.silva24@aluno.senai.br>
2025-04-04 11:01:41 -0300

    Add files via upload

---
+++ Cadastro.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cadastro;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Cadastro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    //Student data
+    String studentName = "Ricardo ";
+    int studentID = 20;
+    int studentAge = 18;
+    float studentfee = 67.34f;
+    String studentGrade = "1DsSeduc";
+    
+    //Print Variables
+    System.out.println("Student Name:" + studentName);
+    System.out.println("Student ID:" + studentID);
+    System.out.println("Student Age:" + studentAge);
+    System.out.println("Student Fee:" + studentfee);
+    System.out.println("Student Grade:" + studentGrade);
+    
+    
+    
+    
+    
+    }
+    
+}
