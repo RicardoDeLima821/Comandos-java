4cc8d2f9aa72b1922b05effb547a049a2a910b26
RicardoDeLima821 <ricardo.l.silva24@aluno.senai.br>
2025-04-04 10:59:33 -0300

    Add files via upload

---
+++ Comandos.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package comandos;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Comandos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Seja Bem-Vindo!");
+       System.out.print("Ricardo");
+       System.out.println("Estou aprendeno no Java");
+       System.out.println("Isso e Incrivel!");
+       System.out.println("Venha Fazer Java Tambem");
+       System.out.println(3);
+       System.out.println(66);
+       System.out.println(999);
+       System.out.println(3+3);
+       System.out.println(2*5);
+       String nome = "Ricardo";
+       System.out.println(nome);
+       int meuNum = 17;
+       System.out.println(meuNum);
+       int anoAtual = 2025;
+       int anoNascimento = 2007;
+       System.out.println(anoAtual - anoNascimento);        
+       System.out.println("ola " + nome);
+       String priNome = "Ricardo";
+       String ultNome = " de lima";
+       String nomeCom = priNome + ultNome;
+       System.out.println(nomeCom);
+       int x = 5, y = 5, z = 5;
+       System.out.println(x + y + z);
+       int x1, y1, z1;
+       x1 = y1 = z1 = 50;
+       System.out.println(x1 + y1 + z1);
+       
+       
+       
+       
+       
+       
+       
+       
+    }
+    
+}
